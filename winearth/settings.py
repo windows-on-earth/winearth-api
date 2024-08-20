bcf90d9fc02bd5d1d1c1d5e8e0086ee2ce55b536
Kevin Coakley <kcoakley@sdsc.edu>
2024-08-20 16:20:20 -0700

    Changed STATIC_URL = 'django-static/'
    Signed-off-by: Kevin Coakley <kcoakley@sdsc.edu>

--- winearth/settings.py
+++ winearth/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'django-static/'
 STATIC_ROOT = "/django/static/"
 
 # Default primary key field type
